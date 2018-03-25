e07933b91ac95ac7d2bf094122faf7141059c9cc
Laura Dietz <dietz@cs.umass.edu>
2018-03-25 13:27:26 -0400

    connecting everything - using bare html interface

--- soot-server.py
+++ soot-server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from soot_backend import *
 
 app = Flask(__name__)
@@ -26,7 +26,7 @@ class FlaskUI(UserInterface):
         self.base_url=args.base_url
         self.user=args.user
         self.password=args.password
-        self.query=args.query
+        self.query='..'
 
     def set_query(self, query):
         self.query = query
@@ -37,12 +37,31 @@ user_interface = FlaskUI()
 user_interface.login()
 
 
+@app.route('/')
+def index():
+    return render_template('index.html', results=[], query=None)
 
-@app.route("/")
-def soot_result():
+@app.route('/query')
+def query():
+    query_raw = request.args['q'].split(" ")
+#     results = []
+#     for toot in toots:
+#         if query in toot['content']:
+#             results.append({
+#                 'content': strip_tags(toot['content']),
+#                 'author': toot['account']['username']
+#             })
+#
+#     return flask.render_template('index.html', toots=results, query=query)
+#
+#
+#
+# @app.route("/")
+# def soot_result():
+
     # Fetch the search query
-    query_raw = "@deeds people"# response.args.query
-    print("searching for terms: ", ", ".join(query_raw))
+    # query_raw = "@deeds people"# response.args.query
+    print("searching for terms: ", query_raw)
     # parameters for the information retrieval model (BM25)
     query_terms = [q.lower() for q in query_raw]  # normalize query terms for searching
 
@@ -55,6 +74,10 @@ def soot_result():
 
     # rank toots by relevance
     scoredToots = searcher.rank(user_interface.getHomeToots(), query_terms)
-    toot_ranking = [{'href':"http://no-idea", 'displayname':toot.get_display_name(), 'caption':toot.get_content()} for (toot,score) in scoredToots]
+    toot_ranking = [{'href':"http://no-idea/", 'caption':toot['content']} for (toot,score) in scoredToots]
+    print(toot_ranking)
+
     return render_template('search-ninja.html', query=" ".join(query_terms), result=toot_ranking)
 
+
+app.run()
