0a8c7fde498cb34e0f114d86488352e49fe89194
Laura Dietz <dietz@cs.umass.edu>
2018-03-25 09:13:43 -0400

    refactoring: wrap retrieval code in BM25 model and Searcher class

--- soot-bm25.py
+++ soot-bm25.py
@@ -67,6 +67,71 @@ class UserInterface():
     def getQuery(self):
         return self.args.query.split(" ")
 
+
+class BM25():
+    '''Implementation of the BM25 retrieval model'''
+
+    def __init__(self, docfreqs, total_toots, avgdl):
+        # BM25 parameters
+        self.avgdl = avgdl
+        self.total_toots = total_toots
+        self.docfreqs = docfreqs
+        self.bm25_b=0.75
+        self.bm25_k1=1.2
+        self.min_query_matches = 1  # adjust how many terms must be in a toot for it to show up in the search
+
+    def bm25_term(self,q,tf, doc_length):
+        ''' Score for a single search term '''
+        df=self.docfreqs[q]
+        idf = math.log(self.total_toots - df + 0.5) - math.log(df + 0.5)
+        term1 = tf * (self.bm25_k1+1.0) / (tf + self.bm25_k1 * (1.0-self.bm25_b + self.bm25_b * doc_length/self.avgdl))
+
+        return idf * term1
+
+    def bm25_score(self, toot, query_terms):
+        ''' Search score for a toot'''
+        text = strip_tags(toot['content']).lower()
+        if sum((1 for q in query_terms if text.count(q) > 0)) >= self.min_query_matches:
+            doc_len = text.count(" ")
+            score = sum((self.bm25_term(q, tf=text.count(q), doc_length=doc_len) for q in query_terms))
+            return score
+        else:
+            return None
+
+
+class Searcher():
+    def __init__(self):
+        pass
+
+    def seed_background_model(self, toots, query_terms):
+        '''collect statistics for the retrieval model.
+        '''
+
+        self.docfreqs = {q:0 for q in query_terms}
+        doc_lens = []
+
+        for toot in toots:
+            text = strip_tags(toot['content'])
+            for q in query_terms:
+                self.docfreqs[q] += text.count(q)
+            doc_lens.append(text.count(" "))
+
+        self.avgdl = sum(doc_lens) / len(doc_lens)
+        self.total_toots = len(user_interface.getHomeToots())
+
+        self.model = BM25(self.docfreqs, self.total_toots, self.avgdl)
+
+    def rank(self, toots, query_terms):
+        # fetch and score toots, kick out dropped toots (None)
+        scoredToots = [(toot, self.model.bm25_score(toot, query_terms)) for toot in toots]
+        scoredToots = [ pair for  pair in scoredToots if pair[1] is not None]
+
+        # sort descendingly by search score
+        scoredToots.sort(key=lambda pair: -pair[1])
+        return scoredToots
+
+
+
 user_interface = UserInterface()
 user_interface.login()
 
@@ -75,63 +140,13 @@ query_raw = user_interface.getQuery()
 print("searching for terms: ", ", ".join(query_raw))
 
 
-
-
 # parameters for the information retrieval model (BM25)
-
-bm25_b=0.75
-bm25_k1=1.2
-
-min_query_matches = 1  # adjust how many terms must be in a toot for it to show up in the search
 query_terms = [q.lower() for q in query_raw]  # normalize query terms for searching
 
+searcher = Searcher()
+searcher.seed_background_model(user_interface.getHomeToots(), query_terms)
 
-
-
-
-# collect statistics for the retrieval model.
-# Here: all fetched toots
-# Should be using public sources
-
-docfreqs = {q:0 for q in query_terms}
-doc_lens = []
-
-for toot in user_interface.getHomeToots():
-    text = strip_tags(toot['content'])
-    for q in query_terms:
-        docfreqs[q] += text.count(q)
-    doc_lens.append(text.count(" "))
-
-avgdl = sum(doc_lens) / len(doc_lens)
-total_toots = len(user_interface.getHomeToots())
-
-
-# Implementation of the BM25 retrieval model
-
-def bm25_term(q,tf, doc_length):
-    ''' Score for a single search term '''
-    df=docfreqs[q]
-    idf = math.log(total_toots - df + 0.5) - math.log(df + 0.5)
-    term1 = tf * (bm25_k1+1.0) / (tf + bm25_k1 * (1.0-bm25_b + bm25_b * doc_length/avgdl))
-
-    return idf * term1
-
-def bm25_score(toot):
-    ''' Search score for a toot'''
-    text = strip_tags(toot['content']).lower()
-    if sum((1 for q in query_terms if text.count(q) > 0)) >= min_query_matches:
-        doc_len = text.count(" ")
-        score = sum((bm25_term(q, tf=text.count(q), doc_length=doc_len) for q in query_terms))
-        return score
-    else:
-        return None
-
-# fetch and score toots, kick out dropped toots (None)
-scoredToots = [(toot, bm25_score(toot)) for toot in user_interface.getHomeToots()]
-scoredToots = [ pair for  pair in scoredToots if pair[1] is not None]
-
-# sort descendingly by search score
-scoredToots.sort(key=lambda pair: -pair[1])
+scoredToots = searcher.rank(user_interface.getHomeToots(), query_terms)
 
 
 # print
