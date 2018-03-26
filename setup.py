021e394c83d4f03ff6f0f4bdbd7a9283861ca7f5
Laura Dietz <dietz@cs.umass.edu>
2018-03-26 00:11:04 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='soot',
+    version='0.5',
+    packages=[],
+    url='https://github.com/laura-dietz/soot',
+    license='',
+    author='laura-dietz',
+    author_email='',
+    description='Soot - search on toots'
+)
